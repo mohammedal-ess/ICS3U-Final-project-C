681a60eb715b6618460aca32032709bf48aecbc2
mohammedal-ess <99287125+mohammedal-ess@users.noreply.github.com>
2022-05-30 14:01:15 -0400

    add save1

---
+++ save1.c
@@ -0,0 +1,23 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+
+//
+
+// Created by: Mohammed al-essawi
+
+// Created on: May 2022
+
+// This program is the "Hello, World!" program for the GameBoy
+
+
+
+#include <stdio.h>
+
+#include <gb/gb.h>
+
+
+
+int main() {
+
+    printf("Hello, mohammed!");
+
+}
